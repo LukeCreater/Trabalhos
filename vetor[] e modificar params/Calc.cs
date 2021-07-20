785af0166c6975cfbe3ef4aaf69c09a3d8b36ec0
Lucas Corrêa <63137745+lucacorrea@users.noreply.github.com>
2021-07-20 20:44:25 -0300

    uso de params, vetor e for

---
+++ vetor[] e modificar params/Calc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course
+{
+    class Calc
+    {
+        public int Somar(params int[] numeros)
+        {
+            int soma = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                soma += numeros[i];
+            }
+            return soma;
+        }
+    }
+}
---
+++ vetor[] e modificar params/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc c = new Calc();
+            int soma = c.Somar(1, 5, 7);
+            Console.WriteLine(soma);
+        }
+    }
+}
