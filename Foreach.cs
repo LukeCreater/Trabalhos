02530bdc2f85f78ea92cb2a75bf304371631d72e
Lucas Corrêa <63137745+lucacorrea@users.noreply.github.com>
2021-07-20 21:33:27 -0300

    uso de foreach em vetor

---
+++ Foreach.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Course
+{
+    class Foreach
+    {
+        static void Main(string[] args)
+        {
+            string[] vetores = { "RJ", "SP", "MG" };
+
+            foreach (string vetor in vetores)
+            {
+                Console.WriteLine(vetor);
+            }
+        }
+    }
+}
